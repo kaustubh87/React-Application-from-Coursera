fc7a35d1730bb216bbb042aa3b13ebb19bf8675c
kvin1087 <kaustubh87@gmail.com>
2018-06-12 16:52:02 -0700

    Added bootstrap 4 navbar

--- restaurant/src/App.js
+++ restaurant/src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import { Navbar } from 'reactstrap';
+import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Navbar dark color = "primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
       </div>
     );
   }
